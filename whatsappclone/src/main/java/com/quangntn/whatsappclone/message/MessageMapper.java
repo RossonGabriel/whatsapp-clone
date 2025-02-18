dc025a892bda7c79692c6137f76c52edcf6c91dd
RossonGabriel <quang.nguyentuongnhat@gmail.com>
2025-02-18 20:54:17 +0700

    implement file service

--- whatsappclone/src/main/java/com/quangntn/whatsappclone/message/MessageMapper.java
+++ whatsappclone/src/main/java/com/quangntn/whatsappclone/message/MessageMapper.java
@@ -1,5 +1,6 @@
 package com.quangntn.whatsappclone.message;
 
+import com.quangntn.whatsappclone.file.FileUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +14,7 @@ public class MessageMapper {
                 .type(message.getType())
                 .state(message.getState())
                 .createdAt(message.getCreatedDate())
-                // todo read the media file
+                .media(FileUtils.readFileFromLocation(message.getMediaFilePath()))
                 .build();
     }
 }
