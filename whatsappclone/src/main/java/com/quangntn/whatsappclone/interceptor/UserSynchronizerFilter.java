8e0fa1e2afb6526bdd68f597a71a06455b6d3c3e
RossonGabriel <quang.nguyentuongnhat@gmail.com>
2025-02-20 16:36:04 +0700

    done backend

--- whatsappclone/src/main/java/com/quangntn/whatsappclone/interceptor/UserSynchronizerFilter.java
+++ whatsappclone/src/main/java/com/quangntn/whatsappclone/interceptor/UserSynchronizerFilter.java
@@ -31,5 +31,6 @@ public class UserSynchronizerFilter extends OncePerRequestFilter {
 
             userSynchronizer.synchronizeWithIdp(token.getToken());
         }
+        filterChain.doFilter(request, response);
     }
 }
